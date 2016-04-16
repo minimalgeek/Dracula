4009e64758cd3c5c56007cb8c0c50a351ccfab19
minimalgeek <minimalgeek@gmail.com>
2016-04-16 21:50:34 +0200

    Level01 alap

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("y", -60f, "loopType", "pingPong", "time", 0.3f));
+    }
+
+    void Update () {
+	
+	}
+}
