08052a1ef56040977d518a72afe4bee0dad3a035
Weakens <tornyosi.m@gmail.com>
2016-04-16 15:32:28 +0200

    Movement+Shapeshift

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    public float rotSpeed=10;
+    CharacterController cc;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            cc.SimpleMove(transform.forward * speed*Time.deltaTime);
+        }
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime, 0);
+
+
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            //if (0!=Input.GetAxis("Horizontal"))
+            //{
+            //    moveDirection = transform.forward*Input.GetAxis("Horizontal"); 
+            //}
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ShapeShiftController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeShiftController : MonoBehaviour {
+
+    public GameObject shapePanel;
+    public enum Shapes {Shape1, Shape2, Shape3, Shape4, Shape5, Shape6};
+
+    Shapes wantedShape;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            shapePanel.SetActive(true);
+            Time.timeScale = 0.15f;
+        }
+        else if (Input.GetButtonUp("Fire2"))
+        {
+            shapePanel.SetActive(false);
+            Time.timeScale = 1;
+            ShapeShift();
+        }
+    }
+
+    public void SetWantedShape(int n)
+    {
+        wantedShape = (Shapes)n;
+    }
+
+    void ShapeShift()
+    {
+        gameObject.layer=LayerMask.NameToLayer(wantedShape.ToString());
+    }
+}
