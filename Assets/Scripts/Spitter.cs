15f1245fe1cb520f06de4462d89fc44fb749894c
minimalgeek <minimalgeek@gmail.com>
2016-04-17 21:06:41 +0200

    Tile 03

--- Assets/Scripts/Spitter.cs
+++ Assets/Scripts/Spitter.cs
@@ -4,18 +4,15 @@ using System.Collections;
 public class Spitter : MonoBehaviour {
 
     public GameObject bullet;
-    public float bulletSpeed = 200;
-    public float cooldown = 3;
     public Transform spawnPoint;
 
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("Fire", Random.Range(0.3f, 1f), cooldown);
+        InvokeRepeating("Fire", Random.Range(0.3f, 1f), Random.Range(1f, 2.5f));
 	}
 	
     public void Fire()
     {
        GameObject g= Instantiate(bullet, spawnPoint.position, spawnPoint.rotation)as GameObject;
-       //g.GetComponent<Rigidbody>().AddForce(g.transform.forward*bulletSpeed);
     }
 }
