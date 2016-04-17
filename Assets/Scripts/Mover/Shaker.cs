5b62482b09b392d628ff977174093355f7cd2c4a
minimalgeek <minimalgeek@gmail.com>
2016-04-17 14:26:36 +0200

    New prefabs

---
+++ Assets/Scripts/Mover/Shaker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shaker : MonoBehaviour {
+
+    public float shakeDistance = 5f;
+    public float shakeTime = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        iTween.ShakePosition(this.gameObject, new Vector3(shakeDistance, shakeDistance, shakeDistance), shakeTime);
+    }
+}
