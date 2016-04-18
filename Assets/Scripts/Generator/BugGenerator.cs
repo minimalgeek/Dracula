80b50d665558ac74604e1774619fd445d971e630
minimalgeek <minimalgeek@gmail.com>
2016-04-18 13:14:05 +0200

    Bug generator

---
+++ Assets/Scripts/Generator/BugGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BugGenerator : MonoBehaviour {
+
+    public GameObject bugToInitialize;
+    public float generateTime = 2f;
+	
+	void Start () {
+        InvokeRepeating("Generate", 0f, generateTime);
+	}
+	
+	private void Generate()
+    {
+        Instantiate(bugToInitialize, transform.position, Quaternion.identity);
+    }
+}
