edc97bfa2691647f17bfe1751bd56373571c8db3
Weakens <tornyosi.m@gmail.com>
2016-04-18 16:43:10 +0200

    sun

---
+++ Assets/Sun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+    Rigidbody rb;
+    public float speed = 50;
+	// Use this for initialization
+	void Start () {
+       rb= GetComponent<Rigidbody>();
+        rb.AddForce(transform.right*speed, ForceMode.Impulse);
+	}
+	
+	void OnTriggerEnter(Collider c)
+    {
+        if (c.tag=="Player")
+        {
+            c.GetComponent<Health>().DamageTaken(500);
+        }
+    }
+}
