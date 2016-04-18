09fc3efe7e540c1b96280eb439139d759eff1b2c
minimalgeek <minimalgeek@gmail.com>
2016-04-18 11:57:07 +0200

    TargetFollow

--- Assets/Scripts/Mover/TargetFollow.cs
+++ Assets/Scripts/Mover/TargetFollow.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class TargetFollow : MonoBehaviour {
+
 	public Transform targetToFollow;
 	public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
+    private Vector3 offset;
+    
+    void Start()
+    {
+        offset = transform.position - targetToFollow.position;
+    }
 
-	public void FixedUpdate () {
-		Vector3 destination = targetToFollow.position;
-		destination.y = transform.position.y;
-		destination.z = transform.position.z;
+    void Update() {
+		Vector3 destination = targetToFollow.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
     }
 }
